0afb59701ee9e539bc4fc4f737206d30d5f15812
maxnoob <67117127+maxnoob@users.noreply.github.com>
2023-10-30 20:01:04 +0100

    data persistence on refresh working! but type error thrown...

--- vue-project/src/components/Vision.vue
+++ vue-project/src/components/Vision.vue
@@ -3,34 +3,38 @@
 
         <h4 class="py-2">Sehvermögen</h4>
 
-        <input type="checkbox" v-model="vision_aid_present"> Sehhilfe vorhanden
+        <input type="checkbox" v-model="vision.aid_present"> Sehhilfe vorhanden
 
         <div class="row d-inline-flex py-2">
             <label>Visus unkorrigiert:</label>
             <div class="row">
                 <div class="col">
-                    <input type="text" placeholder="rechts" v-model="bloodpressure_sys">
+                    <input type="text" placeholder="rechts" v-model="vision.uncorr_re">
                 </div>
                 <div class="col">
-                    <input type="text" placeholder="links" v-model="bloodpressure_dia" style="margin-left: -60px; margin-right: 50px;">
+                    <input type="text" placeholder="links" v-model="vision.uncorr_li" style="margin-left: -60px; margin-right: 50px;">
                 </div>
             </div>
         </div>
 
-        <div v-if="vision_aid_present" id="vision_corrected" class="row d-inline-flex py-2 vision_extended">
+        <div v-if="vision.aid_present" id="vision_corrected" class="row d-inline-flex py-2 vision_extended">
             <div class="col">
                 <label>Visus korrigiert:</label>
             <div class="row">
                 <div class="col">
-                    <input type="text" placeholder="rechts" v-model="bloodpressure_sys">
+                    <input type="text" placeholder="rechts" v-model="vision.corr_re">
                 </div>
             </div>
             <div class="col">
-                <input type="text" placeholder="links" v-model="bloodpressure_dia" >
+                <input type="text" placeholder="links" v-model="vision.corr_li" >
             </div>
             </div>
         </div>
-
+        <select v-model="vision_corr_re">
+            <option value="na"></option>
+            <option value=1>1</option>
+            <option value=1.25>1.25</option>
+        </select>
         <div class="row py-2">
             <div id="vis_field"></div>
         </div>
@@ -55,37 +59,26 @@
 
 </template>
 
-<script>
-import { ref } from 'vue';
-export default {
-    data() {
-        return {
-            vision_aid_present: false
-        }
-    }
-}
-</script>
-
 <script setup>
-
-
-
-
+import { onMounted, ref, watch } from 'vue';
+    const vision = ref({uncorr_re: "", uncorr_li: "", aid_present: false})
+/* populate fields with stored data */
+    onMounted(() => 
+        vision.value = JSON.parse(localStorage.getItem('vision')) || JSON.stringify(this.vision.value)
+    ) // TODO: throws "Uncaught TypeError" after emptied field, but still works...
+    /* use watch to retain inputted data via localStorage */
+    watch(vision, () => {
+        console.log("watcher triggered");
+        localStorage.setItem('vision', JSON.stringify(vision.value));
+            }, { deep: true }, { immediate: true }
+        )
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 input[type=text] {
     width:80px;
 }
-/*   form {
-    max-width: 420px;
-    margin: 30px auto; /* centers */
-    /*background: transparent;
-    text-align: left;
-    padding: 40px;
-    border-radius: 10px;
-  }
- */
 
 </style>
--- vue-project/src/components/Vitals.vue
+++ vue-project/src/components/Vitals.vue
@@ -4,18 +4,18 @@
         <label>Blutdruck:</label>
         <div class="row">
             <div class="col">
-            <input type="text" placeholder="sys mmHg" v-model="bloodpressure_sys">
+            <input type="text" placeholder="sys mmHg" v-model="vitals.bloodpressure_sys">
         </div>
         <div class="col">
-            <input type="text" placeholder="dia mmHg" v-model="bloodpressure_dia" >
+            <input type="text" placeholder="dia mmHg" v-model="vitals.bloodpressure_dia" >
         </div>
         </div>
 
         <label>Puls:</label>
-        <input type="text" placeholder="Schl./min" v-model="pulse">
+        <input type="text" placeholder="Schl./min" v-model="vitals.pulse">
 
         <div>
-            <input type="checkbox" value="regelm" v-model="puls_reg">
+            <input type="checkbox" value="regelm" v-model="vitals.puls_reg">
             <label>regelmässig</label>
         </div>
         <label>Allgemeinzustand:</label>
@@ -32,12 +32,12 @@
         <div class="row">
             <div class="col">
                 <label>Grösse:</label>
-                <input type="text" placeholder="cm" v-model="height">
+                <input type="text" placeholder="cm" v-model="vitals.height">
                 <input type="checkbox" id="height_measured">gemessen
             </div>
             <div class="col">
                 <label>Gewicht:</label>
-                <input type="text" placeholder="kg" v-model="weight" >
+                <input type="text" placeholder="kg" v-model="vitals.weight" >
                 <input type="checkbox" id="weight_measured">gemessen
             </div>
         </div>
@@ -47,7 +47,7 @@
                 <button id="btn_bmi" class="btn btn-secondary btn-sm" @click="bmi_clicked()" style="white-space: nowrap;">BMI berechnen</button>
             </div>
             <div class="col">
-                <div v-if="bmi" style="white-space: nowrap; position:relative">{{ bmi }} kg/m<sup>2</sup> (Norm: 18.5 - 25)</div>
+                <div v-if="vitals.bmi" style="white-space: nowrap; position:relative">{{ vitals.bmi }} kg/m<sup>2</sup> (Norm: 18.5 - 25)</div>
             </div>
         </div>
     </form>
@@ -55,52 +55,30 @@
 </template>
 
 
-<script>
-import { ref } from 'vue';
-export default {
-    data() {
-        return {
-            bloodpressure_sys: '',
-            bloodpressure_dia: '',
+<script setup>
+import { onMounted, ref, watch } from 'vue';
+    const vitals = ref({
+            bloodpressure_sys: "",
+            bloodpressure_dia: "",
             pulse: '',
-            puls_reg:  false
-        }
-    },
-    /* populate fields with stored data */
-    mounted() {
-    this.bloodpressure_sys = JSON.parse(localStorage.getItem('form')) || [];
-  },
-  /* use watch to retain inputted data via localStorage */
-  watch: {
-    bloodpressure_sys: {
-        handler: function() {
-            localStorage.setItem('form', JSON.stringify(this.bloodpressure_sys));
-        },
-        deep: true
-        },
-        form: {
-        handler: function() {
-            localStorage.setItem('form', JSON.stringify(this.bloodpressure_sys));
-        },
-        deep: true
-        }
-    },
-    setup(){
-    const weight = ref("");
-    const height = ref("");
-    const bmi = ref(null);
+            puls_reg:  false,
+            height: '',
+            weight: '',
+            az: true,
+            bmi: null
+        })
     // Calculate BMI
     function calcBMI(weight, height) {
         let bmi = (weight / (height/100)**2).toPrecision(3);
         return bmi
     }
     function bmi_clicked() {
-        console.log(height.value + " | " + weight.value)
-        console.log(typeof weight.value)
+        console.log(vitals.height.value + " | " + vitals.weight.value)
+        console.log(typeof vitals.weight.value)
         let regex = new RegExp("^[0-9]{2,3}$");
         try {
-            if (weight.value.match(regex) && height.value.match(regex)){
-                bmi.value = calcBMI(weight.value, height.value);
+            if (vitals.weight.value.match(regex) && vitals.height.value.match(regex)){
+                vitals.bmi.value = calcBMI(vitals.weight.value, vitals.height.value);
             }
             else {
                 alert("Grösse und Gewicht: Zahlen 10-999)")
@@ -109,38 +87,16 @@ export default {
             alert("Zur BMI-Berechnung Grösse und Gewicht angeben (Zahlen 10-999)")
         }
     }
-    function saveValue() {
-        console.log("change/keyup event triggered")
-        var id = this.id;  // get the sender's id to save it 
-        let val = "";
-        if (this.type == "radio") {
-            val = this.checked; // get status of current radio button
-            let radio_group = this.name;
-            var ele = document.getElementsByName(radio_group); // get all radio buttons from that group
-            for (i = 0; i < ele.length; i++) {
-                if (ele[i] != this) {  // unchecks all other radio buttons in localStorage of same group
-                    ele[i].checked = false;
-                    localStorage.setItem(ele[i].id, false);
-                }
-            }
-        }
-        else if (this.type == "checkbox") {
-            val = this.checked; // get status of current radio button
-        }
-        else if (id == 'output'){
-            val = this.innerText; // to store the generated text
-        }
-        else {
-            val = this.value; // get the value
-        }
-        console.log(val)
-        localStorage.setItem(id, val); // Every time user writing something, the localStorage's value will override
-    }
-    return {
-        weight, height, bmi, bmi_clicked, saveValue
-    }
-}
-}
+    /* populate fields with stored data */
+    onMounted(() => 
+        vitals.value = JSON.parse(localStorage.getItem('vitals')) || JSON.stringify(this.vitals.value)
+    ) // TODO: throws "Uncaught TypeError" after emptied field, but still works...
+    /* use watch to retain inputted data via localStorage */
+    watch(vitals, () => {
+        console.log("watcher triggered");
+        localStorage.setItem('vitals', JSON.stringify(vitals.value));
+            }, { deep: true }, { immediate: true }
+        )
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -148,14 +104,5 @@ export default {
 input[type=text] {
     width:80px;
 }
-/*   form {
-    max-width: 420px;
-    margin: 30px auto; /* centers */
-    /*background: transparent;
-    text-align: left;
-    padding: 40px;
-    border-radius: 10px;
-  }
- */
 
 </style>
