78da400ab7a334fd8076ad3b75b28575f655cbeb
tamaralu12 <tamara.luginbuehl@students.bfh.ch>
2023-12-01 12:47:28 +0100

    subst. columns working, psych now with RadioInputGroup

--- vue-project/src/components/Substance.vue
+++ vue-project/src/components/Substance.vue
@@ -6,65 +6,65 @@
 
   
   
-      <div class="d-flex py-2">
-          <label>Foeter aethylicus:</label>
-          <RadioInputGroup
+      <div class="d-flex py-2 ">
+          <label class="def-label">Foeter aethylicus:</label>
+          <RadioInputGroupColumn
             v-model="substance.foeter"
             name="foeter"
             :options="nein_jaOptions"
           />
         </div>
         <div class="d-flex py-2">
-          <label>Gesichtserythem:</label>
-          <RadioInputGroup
+          <label class="def-label">Gesichtserythem:</label>
+          <RadioInputGroupColumn
             v-model="substance.faceerythem"
             name="faceerythem"
             :options="nein_jaOptions"
           />
         </div>
         <div class="d-flex py-2">
-          <label>Spider naevi:</label>
-          <RadioInputGroup
+          <label class="def-label">Spider naevi:</label>
+          <RadioInputGroupColumn
             v-model="substance.spidernaevi"
             name="spidernaevi"
             :options="nein_jaOptions"
           />
         </div>
         <div class="d-flex py-2">
-          <label>Einstichstellen:</label>
-          <RadioInputGroup
+          <label class="def-label">Einstichstellen:</label>
+          <RadioInputGroupColumn
             v-model="substance.puncture"
             name="puncture"
             :options="nein_jaOptions"
           />
         </div>
         <div class="d-flex py-2">
-          <label>Ikterus:</label>
-          <RadioInputGroup
+          <label class="def-label">Ikterus:</label>
+          <RadioInputGroupColumn
             v-model="substance.icterus"
             name="icterus"
             :options="nein_jaOptions"
           />
         </div>
         <div class="d-flex py-2">
-          <label>Palmarerythem:</label>
-          <RadioInputGroup
+          <label class="def-label">Palmarerythem:</label>
+          <RadioInputGroupColumn
             v-model="substance.palmarerythem"
             name="palmarerythem"
             :options="nein_jaOptions"
           />
         </div>
         <div class="d-flex py-2">
-          <label>Nasenscheidewand:</label>
-          <RadioInputGroup
+          <label class="def-label">Nasenscheidewand:</label>
+          <RadioInputGroupColumn
             v-model="substance.septum"
             name="septum"
             :options="nein_jaOptions"
           />
         </div>
         <div class="d-flex py-2">
-          <label>Narbenstrasse:</label>
-          <RadioInputGroup
+          <label class="def-label">Narbenstrasse:</label>
+          <RadioInputGroupColumn
             v-model="substance.scar"
             name="scar"
             :options="nein_jaOptions"
@@ -76,7 +76,7 @@
   
   <script setup>
   import { onMounted, ref, watch } from "vue";
-  import RadioInputGroup from "./InputComponents/RadioInputGroup.vue";
+  import RadioInputGroupColumn from "./InputComponents/RadioInputGroupColumn.vue";
   import persistToLocalStorage from "@/utils/persistToLocalStorage";
   import FormCard from "./FormCard.vue";
   
@@ -111,5 +111,10 @@ watch(
   { immediate: true }
 );
 </script>
+<style>
+ .def-label {
+  width: 200px;
+}
+</style>
 
   
