d696e8009a850f4be18b43f61ce261f2b81c625f
maxnoob <67117127+maxnoob@users.noreply.github.com>
2023-12-08 10:30:06 +0100

    bootstrap icon uncommented

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
-import "./libraries/bootstrap-icons.css"
+//import "./libraries/bootstrap-icons.css"
 
 createApp(App).mount('#app')
 // remember, each Vue plugin needs its own .use()
