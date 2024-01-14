0b07e263ff865e7be532cbc0a95bf9c1d64705fc
maxnoob <67117127+maxnoob@users.noreply.github.com>
2024-01-14 21:34:46 +0100

    changed config file

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
-module.exports = {
+module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/lc1-traffic-docu/'
     : '/'
-}
+})
