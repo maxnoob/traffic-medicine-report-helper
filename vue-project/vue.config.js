907187979b79993ba3246ba187cd4c9d100df5f2
maxnoob <67117127+maxnoob@users.noreply.github.com>
2024-01-14 21:13:51 +0100

    creating distro for  project submission

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/lc1-traffic-docu/vue-project/'
+    : '/'
+}
